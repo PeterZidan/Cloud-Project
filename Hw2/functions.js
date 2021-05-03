0aa29b873e8aebdd6b0afe89961d29ba295b16a9
PeterZidan <peterzidan30@gmail.com>
2021-05-03 12:32:38 +0300

    a

---
+++ Hw2/functions.js
@@ -0,0 +1,89 @@
+function homeFunction() {
+    window.location.href = "index.html";
+}
+function buyFunction() {
+    window.location.href = "buy.html";
+}
+function sellFunction() {
+    window.location.href = "sell.html";
+}
+function repairFunction() {
+    window.location.href = "repair.html";
+}
+function contactFunction() {
+    window.location.href = "contact-us.html";
+}
+
+
+function sendFunction() {
+
+
+    var brand = document.getElementById("brand").value;
+    var color = document.getElementById("color").value;
+    var size = document.getElementById("size").value;
+    var capacity = document.getElementById("capacity").value;
+    var firstN = document.getElementById("firstname").value;
+    var lastN = document.getElementById("lastname").value;
+    var phoneN = document.getElementById("phonenumber").value;
+    console.log(5);
+    console.log(brand);
+    console.log(color);
+    console.log(size);
+    
+    
+    if (brand == "" || color == "" || size == "" || capacity == "" || firstN == "" || lastN == "" || phoneN == "") {
+
+        alert('Fill All the missing data please!!');
+        
+    }
+    else {
+        alert('Your Data has been sent successfully, we will contact you as soon as it is possible ');
+        
+    }
+}
+
+
+function send1Function() {
+
+
+    var brand = document.getElementById("brand1").value;
+    var color = document.getElementById("color1").value;
+    var size = document.getElementById("size1").value;
+    var capacity = document.getElementById("capacity1").value;
+    var firstN = document.getElementById("firstname1").value;
+    var lastN = document.getElementById("lastname1").value;
+    var phoneN = document.getElementById("phonenumber1").value;
+   
+
+
+    if (brand == "" || color == "" || size == "" || capacity == "" || firstN == "" || lastN == "" || phoneN == "") {
+
+        alert('Fill All the missing data please!!');
+
+    }
+    else {
+        alert('Your Data has been sent successfully, we will contact you as soon as it is possible ');
+
+    }
+}
+
+
+
+function send2Function() {
+
+
+    var txt = document.getElementById("txtarea").value;
+ 
+    console.log(txt);
+
+    if (txt == "" ) {
+
+        alert('Fill All the missing data please!!');
+
+    }
+    else {
+        alert('Your Data has been sent successfully, we will contact you as soon as it is possible ');
+
+    }
+}
+
